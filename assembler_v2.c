48b3909fa958a0dee5094a8b980851fde50408fe
Dominic Aschauer <dominic.aschauer@radancy.com>
2023-09-29 12:03:15 +0200

    Implement new tokenizer

--- assembler_v2.c
+++ assembler_v2.c
@@ -1,9 +1,99 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "diatom.h"
 #include "util.h"
+
+#define TOKEN_MAX 16
+#define LINE_MAX 90
+
+struct tokenizer {
+  FILE *file;
+  char token[TOKEN_MAX];
+  char line_buffer[LINE_MAX];
+  unsigned int line_number;
+};
+
+// next_line reads the next line from the input file. It returns the
+// length of the read line, 0 if EOF has been reached or -1 if an
+// error occured.
+static int next_line(struct tokenizer *t) {
+  assert(t != NULL);
+
+  int err = 0;
+  char *line = NULL;
+  size_t line_cap = 0;
+  ssize_t line_len = 0;
+
+    t->line_number++;
+    line_len = getline(&line, &line_cap, t->file);
+    if (line_len == -1) {
+      if (feof(t->file)) return 0;
+
+      return dlt_errorf("line %d: error while reading line",
+			t->line_number);
+    }
+
+
+  if ((unsigned long)line_len > sizeof(t->line_buffer)) {
+    err = dlt_errorf("line %d: identifier '%s' exceeds max length",
+		     t->line_number, line);
+    goto cleanup;
+  }
+
+  size_t copy_len = strlcpy(t->line_buffer, line, sizeof(t->line_buffer));
+  if (copy_len < (unsigned long)line_len) {
+    err = dlt_errorf("line %d: failed to copy line to buffer",
+		     t->line_number);
+    goto cleanup;
+  }
+
+ cleanup:
+  free(line);
+  if (err) return err;
+  
+  return copy_len;
+}
+
+// next_token reads the token from the input file. It returns the
+// length of the read token, 0 if EOF has been reached or -1 if an
+// error occured.
+static int next_token(struct tokenizer *t) {
+  assert(t != NULL);
+  
+  if (t->token[0] != '\0') return 0;
+
+  char *token = "\0";
+  static char *line = NULL;
+  
+  while (true) {
+    if (line == NULL) {
+      int line_len = next_line(t);
+      if (line_len <= 0) return line_len;
+
+      line = t->line_buffer;
+      continue;
+    }
+    token = strsep(&line, " \t\n\0");
+
+    size_t token_len = strnlen(token, TOKEN_MAX);
+    if (token_len > 0) {
+      size_t copy_len = strlcpy(t->token, token, sizeof(t->token));
+      if (copy_len < (unsigned long)token_len)
+	return dlt_errorf("line %d: failed to copy token", t->line_number);
+
+      return token_len;
+    }
+  }
+}
+
+static void consume_token(struct tokenizer *t) {
+  t->token[0] = '\0';
+}
 
 static void usage(void) {
   puts("Usage: dasm [dasm-file]\n");
@@ -30,7 +120,25 @@ int main(int argc, char* argv[]) {
   }
 
   char *dasm_filename = argv[1];
+  FILE* input_file = fopen(dasm_filename, "r");
+  if (input_file == NULL)
+    dlt_fatal_error("failed to open input file");
+  
+  struct tokenizer t = (struct tokenizer) {
+    .file = input_file,
+    .token = {'\0'},
+    .line_buffer = {'\0'},
+    .line_number = 0,
+  };
 
+  while (next_token(&t) > 0) {
+    printf("line %d: '%s'\n", t.line_number, t.token);
+    consume_token(&t);
+  }
+
+  dlt_panic_on_error();
+
+  fclose(input_file);
 
   return EXIT_SUCCESS;
 }
--- util.h
+++ util.h
@@ -1,6 +1,7 @@
 #ifndef DELTO_UTIL
 #define DELTO_UTIL
 
+#include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -8,28 +9,28 @@
 
 
 /* Error handling */
-#define ERR_MSG_MAX 255
+#define ERR_MSG_MAX 256
 static char error_msg[ERR_MSG_MAX] = "";
 
 int dlt_error(char msg[ERR_MSG_MAX]) {
-    strlcpy(error_msg, msg, sizeof(error_msg));
-    return -1;
+  strlcpy(error_msg, msg, sizeof(error_msg));
+  return -1;
 }
 
 void dlt_panic(void) {
-    fputs("\033[31mError: ", stderr);
-    perror(error_msg);
-    fputs("\033[0m", stderr);
-    exit(EXIT_FAILURE);
+  fputs("\033[31mError: ", stderr);
+  perror(error_msg);
+  fputs("\033[0m", stderr);
+  exit(EXIT_FAILURE);
 }
 
 void dlt_fatal_error(char msg[100]) {
-    dlt_error(msg);
-    dlt_panic();
+  dlt_error(msg);
+  dlt_panic();
 }
 
 void dlt_panic_on_error(void) {
-    if (error_msg[0] != '\0') dlt_panic();
+  if (error_msg[0] != '\0') dlt_panic();
 }
 
 int dlt_errorf(const char * restrict format, ...) {
@@ -54,9 +55,25 @@ bool dlt_string_starts_with(char *s, char *prefix) {
 
 /* Random utility functions */
 double dlt_clamp_value(double value, double min, double max) {
-    if (value < min) return min;
-    else if (value > max) return max;
-    else return value;
+  if (value < min) return min;
+  else if (value > max) return max;
+  else return value;
+}
+
+#define STR_MAX 256
+void dlt_trim_string(char *string) {
+  if (string == NULL || strnlen(string, STR_MAX) == 0) return;
+
+  const char* start = string;
+  while(isspace(*start) || *start == '\n' || *start == '\r') ++start;
+
+  size_t len = strnlen(start, STR_MAX);
+  memmove(string, start, len + 1);
+
+  char* end = string + len - 1;
+  while(end >= string  && (isspace(*end) || *end == '\n' || *end == '\r')) --end ;
+
+  *++end = '\0';
 }
 
 #endif
